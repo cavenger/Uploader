5c3fc3fd8a327947b7ffd1a0584110ab3b93e1f4
Chris Cameron <chriscamero@me.com>
2011-10-11 22:45:37 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoapixUpload::Application.config.secret_token = 'c26226db32798616969ffc68fd72c522077a13babd00ce1e560fec6c7eb70b00e044e7e57d460843b8e66febf9e6044e8e667e196b55b348dc86a1152fd43488'
